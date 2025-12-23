b03bc1c22b4ca1d10da600bc0b11e6a5df818200
iamprasad <durgaprasadkalepuu@gmail.com>
2025-12-23 21:31:04 +0530

    Fix typo in hello function return string

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello DevOps! My Flask app is containerized"
+    return "Hello DevOps! My Flask app is containerized."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
